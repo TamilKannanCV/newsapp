7b3bec588b02d9e395bbf8ceed3ec16805401efa
TamilKannanCV <tamilkk100@gmail.com>
2022-03-31 08:25:50 +0530

    updated

--- android/app/src/main/kotlin/com/newsapp/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.newsapp.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/tk/newsapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tk.newsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
